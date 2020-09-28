21548bc73fb1e66a87f070faa0189f84a06ab42d
vzabavski <vzabavski123@gmail.com>
2020-09-28 15:19:13 +0300

    feat: cat-count task done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
+  backyard = backyard.join().split(',');
+  let sum = 0;
+  for (let x of backyard) {
+    if (x === '^^') {
+      sum++
+    }
+  }
+  return sum;
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(Array) {
+  
+    var team = '';
+    let u = typeof(Array)
+  if (u === 'object') {
+      Array = Array.sort()
+      for (let name of Array) {
+      var a = typeof(name);
+      
+      if (a === "string" && name !== ' ' ){
+        name = name.trim().slice(0, 1).toUpperCase();
+        team += name
+      
+      } else {
+        console.log(false)
+        }
+    }
+    return team
+  } else {
+    return false
+  }
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
