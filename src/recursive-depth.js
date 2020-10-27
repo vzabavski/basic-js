35d291be772ff7a23bd093d557182f6bf255c69b
vzabavski <vzabavski123@gmail.com>
2020-10-27 22:51:03 +0300

    feat: add calculateDepth function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-    
+  calculateDepth(arr, depth = 1, maxDepth = 1) {
+    for(let i = 0; i < arr.length; i++) {
+      if(Array.isArray(arr[i])) {
+        const newDepth = depth + 1
+        maxDepth = this.calculateDepth(arr[i], newDepth, newDepth > maxDepth ? newDepth : maxDepth)
+      }
+    }
+    return maxDepth;
   }
 };
